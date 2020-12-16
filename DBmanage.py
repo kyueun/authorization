bb3ebe6d41d01f3287c0e6a86abf8dc53c8f1960
gyudong <kelee0812.2@gmail.com>
2020-12-16 17:34:59 +0900

    add find, login, register functions

--- DBmanage.py
+++ DBmanage.py
@@ -1,4 +1,5 @@
 import pymysql
+from error import *
 
 
 def get_connection():
@@ -7,15 +8,78 @@ def get_connection():
     return conn
 
 
-def func():
+def find_user(index=None, email=None, name=None):
     conn = get_connection()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-    sql = ''
-    cursor.execute(sql)
-    result = cursor.fetchall()
+    result = None
+
+    if index is not None:
+        sql = 'select * from user where index=%s'
+        cursor.execute(sql, [index])
+        result = cursor.fetchone()
+
+    elif email is not None:
+        sql = 'select * from user where email=%s'
+        cursor.execute(sql, [email])
+        result = cursor.fetchone()
+
+    elif name is not None:
+        sql = 'select * from user where name=%s'
+        cursor.execute(sql, [name])
+        result = cursor.fetchall()
+
+    if result is None:
+        cursor.close()
+        conn.close()
+
+        raise no_user
 
     conn.commit()
 
     cursor.close()
     conn.close()
+
+    return result
+
+
+def register(email, pw, name):
+    try:
+        usr = find_user(email=email)
+
+        raise duplicate_user
+
+    except Exception as e:
+        if e is no_user:
+            pass
+
+        else:
+            raise disable
+
+    conn = get_connection()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    sql = 'insert into user(email, hashed_pw, name) values({email}, {hashed_pw}, {name})'\
+        .format(email=email, hashed_pw=pw, name=name)
+    cursor.execute(sql)
+    result = cursor.fetchone()
+
+    if result is None:
+        cursor.close()
+        conn.close()
+
+        raise disable
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+def login(email=None, pw=None):
+    usr = find_user(email=email)
+
+    if pw != usr['hashed_pw']:
+        raise no_user
+
+    return
